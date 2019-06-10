94750c3254268a078e6b110786a2f232d8335e64
yuda85 <yuda8855@gmail.com>
2019-06-10 22:19:01 +0300

    change

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+//https://warm-waters-16265.herokuapp.com/ | 
+//https://git.heroku.com/warm-waters-16265.git
+
 const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hi: 'there my man'});
 })
 
 const PORT = process.env.PORT || 5000
