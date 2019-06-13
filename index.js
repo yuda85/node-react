587154930e5bf7a7871cd328c2d82d03ae11c7d3
yuda85 <yuda8855@gmail.com>
2019-06-13 22:15:05 +0300

    setting heroku env vars and finish auth flow

--- index.js
+++ index.js
@@ -2,12 +2,29 @@
 //https://git.heroku.com/warm-waters-16265.git
 
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys')
+require('./models/User')
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI)
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there my man'});
-})
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = process.env.PORT || 5000
+require('./routes/authRouts')(app)
+ 
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
